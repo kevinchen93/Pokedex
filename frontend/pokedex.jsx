e8d1ab84344dabaa41b6b9c2bea2cbe8882df531
Kevin Chen <xkevinchen@gmail.com>
2018-12-20 16:52:57 -0500

    Set up frontend structure

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import Root from './components/root';
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Pokedex</h1>, root);
+  const store = configureStore();
+
+  window.state = store.getState;
+  window.dispatch = store.dispatch;
+
+  ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+import { merge } from 'lodash';
+
+const pokemonReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const newState = merge({}, state);
+
+  switch (action.type) {
+    case RECEIVE_ALL_POKEMON:
+      return merge(newState, action.pokemon);
+    default:
+      return state;
+  }
+};
+
+export default pokemonReducer;
